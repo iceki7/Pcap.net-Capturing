563085332e4d7ef9b805d6a4aca02a2dd0f73e90
lceki <1300953421@qq.com>
2022-03-17 15:40:33 +0800

    添加项目文件。

---
+++ Capturing Demo/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using PcapDotNet.Core;
+using PcapDotNet.Packets;
+using PcapDotNet.Packets.IpV4;
+using PcapDotNet.Packets.Transport;
+
+namespace InterpretingThePackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Retrieve the device list from the local machine
+            IList<LivePacketDevice> allDevices = LivePacketDevice.AllLocalMachine;
+
+            if (allDevices.Count == 0)
+            {
+                Console.WriteLine("No interfaces found! Make sure WinPcap is installed.");
+                return;
+            }
+
+            // Print the list
+            for (int i = 0; i != allDevices.Count; ++i)
+            {
+                LivePacketDevice device = allDevices[i];
+                Console.Write((i + 1) + ". " + device.Name);
+                if (device.Description != null)
+                    Console.WriteLine(" (" + device.Description + ")");
+                else
+                    Console.WriteLine(" (No description available)");
+            }
+
+            int deviceIndex = 0;
+            do
+            {
+                Console.WriteLine("Enter the interface number (1-" + allDevices.Count + "):");
+                string deviceIndexString = Console.ReadLine();
+                if (!int.TryParse(deviceIndexString, out deviceIndex) ||
+                    deviceIndex < 1 || deviceIndex > allDevices.Count)
+                {
+                    deviceIndex = 0;
+                }
+            } while (deviceIndex == 0);
+
+            // Take the selected adapter
+            PacketDevice selectedDevice = allDevices[deviceIndex - 1];
+
+            // Open the device
+            using (PacketCommunicator communicator =
+                selectedDevice.Open(65536,                                  // portion of the packet to capture qwe65536
+                                                                            // 65536 guarantees that the whole packet will be captured on all the link layers
+                                    PacketDeviceOpenAttributes.Promiscuous, // promiscuous mode
+                                    1000))                                  // read timeout
+            {
+                // Check the link layer. We support only Ethernet for simplicity.
+                if (communicator.DataLink.Kind != DataLinkKind.Ethernet)
+                {
+                    Console.WriteLine("This program works only on Ethernet networks.");
+                    return;
+                }
+
+                // Compile the filter
+                using (BerkeleyPacketFilter filter = communicator.CreateFilter("udp"))
+                {
+                    // Set the filter
+                    communicator.SetFilter(filter);
+                }
+
+                Console.WriteLine("Listening on " + selectedDevice.Description + "...");
+
+                // start the capture
+                communicator.ReceivePackets(0, PacketHandler);
+            }
+        }
+
+        // Callback function invoked by libpcap for every incoming packet
+
+        /*
+         这两个函数都有一个回调参数，packet_handler，指向将接收数据包的函数。libpcap 为来自网络的每个新数据包调用此函数，
+        并接收通用状态（对应于 pcap_loop（） 和 pcap_dispatch（）的用户参数），这是一个标头，其中包含数据包上的一些信息，
+        如时间戳、长度和数据包的实际数据，包括所有协议标头。请注意，帧 CRC 通常不存在，因为在帧验证后，网络适配器会将其删除。
+        另请注意，大多数适配器会丢弃具有错误 CRC 的数据包，因此 WinPcap 通常无法捕获它们。
+         */
+        private static void PacketHandler(Packet packet)
+        {
+            // print timestamp and length of the packet
+            Console.WriteLine(packet.Timestamp.ToString("yyyy-MM-dd hh:mm:ss.fff") + " length:" + packet.Length);
+
+            IpV4Datagram ip = packet.Ethernet.IpV4;
+            UdpDatagram udp = ip.Udp;
+            String data = ip.Udp.Payload.Decode(System.Text.Encoding.ASCII);
+
+            // print ip addresses and udp ports
+            Console.WriteLine(ip.Source + ":" + udp.SourcePort + " -> " + ip.Destination + ":" + udp.DestinationPort+"| data= "+data);
+        }
+    }
+}
